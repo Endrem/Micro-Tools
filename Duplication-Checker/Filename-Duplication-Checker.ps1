02e91be9ef922ddaf4f74b37b9674df1599c5338
Endrem <github@endrem.com>
2021-01-20 01:32:35 -0500

    Created new filename checker

--- Duplication-Checker/Filename-Duplication-Checker.ps1
+++ Duplication-Checker/Filename-Duplication-Checker.ps1
@@ -1,39 +1,11 @@
-# Get all files (recursively) within the directory we want to scan.
-$files = Get-ChildItem -Path "C:\Users\Admin\Desktop\Dupe-Test" -Recurse -File
-# Ordered hashtable that holds all of the duplicates.
-$masterdupe = [ordered]@{}
+# Get each file within the chosen directory, resursively.
+$Files = Get-ChildItem -Path "C:\Users\Admin\Desktop\Dupe-Test" -Recurse -File
 
-foreach($file in $files)
-{
-    $filename = ($file.BaseName + $file.Extension)
+# Group-Object is used to retrieve a count on all filenames.  The filenames with counts greater than 1 (duplicates) are kept.
+$Duplicates = $Files | Group-Object -Property Name -NoElement | Where-Object -Property Count -GT 1
 
-    # Saves steps by ignoring filenames that have already been checked.
-    if($masterdupe.Contains($filename)){continue}
+# Only keep files whos filenames match duplicate filenames.  Sort to keep the output organized.
+$Output = $Files | Where-Object {$_.Name -in $Duplicates.Name} | Sort-Object -Property BaseName | Select-Object -Property Name, FullName
 
-    $duplicates = @()
-    foreach($dupefile in $files)
-    {
-        # If this is a match, it will add the path to the 'duplicates' array.
-        if($filename -eq ($dupefile.BaseName + $dupefile.Extension))
-        {
-            $duplicates += $dupefile
-        }
-    }
-
-    # If more than one (aka duplicates), the duplicates array is added to the hashtable using the Key $filename.
-    if($duplicates.Count -gt 1)
-    {
-        $masterdupe.Add($filename, $duplicates)
-    }
-}
-# Clear Results.txt before saving contents.
-Clear-Content -Path 'Results.txt'
-
-# Save the results by iterating through the Value of each Key.
-$masterdupe.Keys | ForEach-Object{ Add-Content 'Results.txt' "$_ :`n`t $($masterdupe.$_)" }
-
-# For saving the content as a CSV, use the following code:
-# $masterdupe.GetEnumerator() | 
-#     Select-Object -Property @{N='Filename';E={$_.Key}},
-#     @{N='Duplicate Paths';E={$_.Value}} | 
-#         Export-Csv -NoTypeInformation -Path 'Results.csv'
+# Save output to file.
+Set-Content -Path "Results.txt" -Value ($Output | Out-String)
